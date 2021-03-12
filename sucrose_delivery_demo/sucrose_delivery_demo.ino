d797888e0644160dc1f6a0731ddbc3d6e768676f
jmdelahanty <jdelahanty@salk.edu>
2021-03-11 18:53:41 -0800

    Add airpuff control and lick pins

--- sucrose_delivery_demo/sucrose_delivery_demo.ino
+++ sucrose_delivery_demo/sucrose_delivery_demo.ino
@@ -3,19 +3,24 @@
 // Adapted from DISC_V7.ino by Kyle Fischer and Mauri van der Huevel Oct. 2019
 // NOTE 5/8/21: Testing is being done with water.
 
+//// PACKAGES ////
+#include <Adafruit_MPR121.h> // used for capacitive sensors to detect licks
+#include <digitalWriteFast.h> // speeds up reading/writing to digital pins
+#include <Wire.h> // enhances comms with MPR121
+
 
 //// PIN ASSIGNMENT ////
 // input
-// none established yet
-// plan on adding lick sensing and pressure sensing for airpuff
+const int lickPin = 2; // measure lick responses from mouse, not yet implemented
+// const int airPin = 3; // measure delay from solenoid to mouse, not yet implemented.
 // output
-// const int solPin_air = 13; // solenoid for air puff control
+const int solPin_air = 13; // solenoid for air puff control
 const int solPin_liquid = 12; // solenoid for liquid control: sucrose, water, EtOH
 const int vacPin = 11; // solenoid for vacuum control
 
 // NIDAQ input
 // none established yet
-// using template from DISC_V7, credited above
+// using template from DISC_V7
 // const int NIDAQ_READY = 9;
 // // NIDAQ output
 // const int USDeliveryPos = 10;
@@ -51,26 +56,28 @@ const int vacPin = 11; // solenoid for vacuum control
 
 void setup() {
 /*
-   Sucrose Demo
-   Turns on solenoid for sucrose quickly, activate vacuum, and do so repeatedly.
+   Stimulation Demo
+   Turns on solenoid for air puff, sucrose, and vacuum. Do so repeatedly.
 */
    
 // -- INITIALIZE PINS -- //
-//    pinMode(solPin_air, OUTPUT);
+   pinMode(solPin_air, OUTPUT);
    pinMode(solPin_liquid, OUTPUT);
    pinMode(vacPin, OUTPUT);
 }
 
 
 void loop() {
-//    digitalWrite(solPin_air, HIGH); // turn the LED on (HIGH is the voltage level)
-   digitalWrite(solPin_liquid, HIGH); // open solenoid for liquid
+    digitalWrite(solPin_air, HIGH); // turn the LED on (HIGH is the voltage level)
+   delay(25) // wait 25 ms
+   digitalWrite(solPin_air, LOW); // close air solenoid
+   digitalWrite(solPin_liquid, HIGH); // open liquid solenoid
    delay(5); // wait 5 ms, any longer the water droplet falls, any shorter the solenoid doesn't open
    digitalWrite(solPin_liquid, LOW); // close solenoid for liquid
    delay(800); // wait 800 ms, any longer the droplet falls
-   digitalWrite(vacPin, HIGH); // open solenoid for vacuum
+   digitalWrite(vacPin, HIGH); // open vacuum solenoid
    delay(3000); //wait 3 seconds
-   digitalWrite(vacPin, LOW); // close solenoid for vacuum
-   delay(5000); // wait for a second
+   digitalWrite(vacPin, LOW); // close vacuum solenoid
+   delay(5000); // wait for five seconds
 
 }
