b56f3f8bc5a4055a73cde603ef0d5426ab9bcf1c
Jeremy Delahanty <jdelahanty@salk.edu>
2021-04-13 20:08:27 -0700

    Update python_writing_test.py

--- python_writing_test.py
+++ python_writing_test.py
@@ -3,19 +3,43 @@ import random
 from numpy.random import default_rng
 from pySerialTransfer import pySerialTransfer as txfer
 
+# Define Total Number of Trials
+total_number_trials = 10
+
+# TODO: Send these experiment variables over Serial Comms
+# Define Noise Duration
+noise_duration = 2000 # 2s
+
+# Define Sucrose Delivery Time
+sucrose_delivery_time = 5 # 5ms
+
+# Define Airpuff Delivery Time
+airpuff_delivery_time = 10 # 10ms
+
+# Define Sucrose Consumption Time
+sucrose_consumption_time = 1000 # 1s
+
+# TODO: Generate pseudorandom trials with Aneesh's script
 # Define trial types: 0 = airpuff, 1 = sucrose
 trial_types = [0,1]
 # Define probabilities for each value: 50%, 50%
 probs = [0.5, 0.5]
 
 # Per numpy documentation, use default_rng() as generator
-rng = default_rng(20)
+rng = default_rng(1)
 # Create trials_array using .choice method, 20 instances, allow replacement,
 # use linked probabilities
-trials_array = rng.choice(trial_types, 20, replace=True, p=probs)
+trials_array = rng.choice(trial_types, p=probs, total_number_trials, replace=True)
 
 # default_rng.choice() creates numpy.ndarray; must be list for txfer
 trials_array = trials_array.tolist()
+
+# Define Base ITI
+base_iti = 3000 # 3s
+
+# Define Jitter
+
+
 
 print("Trial List Created")
 
@@ -64,71 +88,3 @@ if __name__ == '__main__':
             link.close()
         except:
             pass
-
-# if __name__ == '__main__':
-#     # print("Starting tx")
-#     try:
-#         link = txfer.SerialTransfer("COM12", 115200)
-#
-#         link.open()
-#         time.sleep(2) # give time for Arudino to reset
-#
-#         while True:
-#             send_size = 0
-#
-#             ###################################################################
-#             # Send a list
-#             ###################################################################
-#             list_ = trials_array
-#             list_size = link.tx_obj(list_)
-#             send_size += list_size
-#             link.send(send_size)
-#
-#             while not link.available():
-#                 if link.status <0:
-#                     if link.status == txfer.CRC_ERROR:
-#                         print("ERROR: CRC_ERROR")
-#                     elif link.status == txfer.PAYLOAD_ERROR:
-#                         print("ERROR: PAYLOAD_ERROR")
-#                     elif link.status == txfer.STOP_BYTE_ERROR:
-#                         print("ERROR: STOP_BYTE_ERROR")
-#                     else:
-#                         print("ERROR: {}".format(link.status))
-#             ###################################################################
-#             # Parse response list
-#             ###################################################################
-#                 rec_list_  = link.rx_obj(obj_type=type(list_),
-#                                         obj_byte_size=list_size,
-#                                         list_format='i')
-#             ###################################################################
-#             # Display the received data
-#             ###################################################################
-#             print('SENT: {}'.format(list_))
-#             print('RCVD: {}'.format(rec_list_))
-#             print(' ')
-#
-#             if list_ == rec_list_:
-#                 print("Confirmed!")
-#                 try:
-#                     link.close()
-#                     False
-#                 except:
-#                     print("Error!")
-#                     break
-#             # TODO: Add packet checking and retry if there's an error in comms
-#
-#
-#     except KeyboardInterrupt:
-#         try:
-#             link.close()
-#         except:
-#             pass
-#
-#     except:
-#         import traceback
-#         traceback.print_exc()
-#
-#         try:
-#             link.close()
-#         except:
-#             pass
