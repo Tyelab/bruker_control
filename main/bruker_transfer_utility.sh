3fa5657970ba1b33c8413b68fc148ecb52244de6
Jeremy Delahanty <jdelahanty@salk.edu>
2021-11-18 18:59:12 -0800

    Update bruker_transfer_utility.sh
    Revert update to transfer utility for _DATA file server directories. Didn't push to main apparently...

--- main/bruker_transfer_utility.sh
+++ main/bruker_transfer_utility.sh
@@ -5,17 +5,10 @@ echo "Bruker File Transfer Utility"
 echo ""
 
 # Put back to user the arguments supplied
-echo "Team: " $1
-echo "Project: " $2
+echo "Project: " $1
 
 # Change to the team's raw file directory
-cd /drives/e/teams/$1
-
-filedatereg=$( echo $file | cut -d '/' -f 2 | cut -d '_' -f 1 )
-filesubjectreg=$( echo $file | cut -d '/' -f 2 | cut -d '_' -f 2 )
-
-dirdatereg=$( echo $directory | cut -d '/' -f 2 | cut -d '_' -f 1 )
-dirsubjectreg=$( echo $directory | cut -d '/' -f 2 | cut -d '_' -f 2 )
+cd /drives/e/$1
 
 echo ""
 echo "Transferring Configuration Files..."
@@ -25,9 +18,9 @@ for file in config/*
     do
         if [[ -f $file ]]; then
             echo "Copying: " $file
-            date=$datereg
-            subject=$subjectreg
-            rsync -P $file /drives/x/raw/$1/$2/2p/$subject/$date
+            date=$(echo $file | cut -d '/' -f 2 | cut -d '_' -f 1 )
+            subject=$(echo $file | cut -d '/' -f 2 | cut -d '_' -f 2 )
+            rsync -P --remove-source-files $file /drives/x/_DATA/$1/2p/raw/$subject/$date
         else
             echo "No configuration files found!"
         fi
@@ -43,7 +36,7 @@ for file in video/*
             echo "Copying: " $file
             date=$datereg
             subject=$subjectreg
-            rsync -P $file /drives/x/raw/$1/$2/2p/$subject/$date
+            rsync -P --remove-source-files $file /drives/x/_DATA/$1/2p/raw/$subject/$date
         else
             echo "No video files found!"
         fi
@@ -57,9 +50,9 @@ for directory in zstacks/*
     do
         if [[ -d $directory ]]; then
             echo "Copying: " $directory
-            date=$dirdatereg
-            subject=$dirsubjectreg
-            rsync -rP $directory /drives/x/raw/$1/$2/2p/$subject/$date/zstacks
+            date=$(echo $directory | cut -d '/' -f 2 | cut -d '_' -f 1 )
+            subject=$(echo $directory | cut -d '/' -f 2 | cut -d '_' -f 2 )
+            rsync -rP --remove-source-files $directory /drives/x/_DATA/$1/2p/raw/$subject/$date/zstacks
         else
             echo "No z-stacks found!"
         fi
@@ -73,9 +66,9 @@ for directory in microscopy/*
     do
         if [[ -d $directory ]]; then
             echo "Copying: " $directory
-            date=$dirdatereg
-            subject=$dirsubjectreg
-            rsync -rP $directory /drives/x/raw/$1/$2/2p/$subject/$date
+            date=$(echo $directory | cut -d '/' -f 2 | cut -d '_' -f 1 )
+            subject=$(echo $directory | cut -d '/' -f 2 | cut -d '_' -f 2 )
+            rsync -rP --remove-source-files $directory /drives/x/_DATA/$1/2p/raw/$subject/$date/
         else
             echo "No t-series found!"
         fi
@@ -86,3 +79,4 @@ echo "Transfers complete!"
 echo ""
 
 echo "File transfer to server complete!" | Mail -s "bruker_transfer_utility" acoley@salk.edu
+echo "File transfer to server complete!" | Mail -s "bruker_transfer_utility" jdelahanty@salk.edu
