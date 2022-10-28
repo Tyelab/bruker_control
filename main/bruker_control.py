a20a83187f576c94f53478fc0af49cb452c51ab7
Jeremy Delahanty <jdelahanty@salk.edu>
2022-10-27 20:49:55 -0700

    Partial documentation update for take flight

--- main/bruker_control.py
+++ main/bruker_control.py
@@ -6,7 +6,7 @@
 # https://github.com/PowerBroker2/pySerialTransfer
 # Genie Nano manufactured by Teledyne DALSA
 
-__version__ = "1.11.4 Preschool"
+__version__ = "1.11.4 Take Flight"
 
 # Import experiment utils to run different experiments
 import experiment_utils
